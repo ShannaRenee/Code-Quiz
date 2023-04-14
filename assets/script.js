35a436cc7b0970e879132eb37f234cc52ed2eca3
Calcifer <shanna.renee.morris@gmail.com>
2023-04-13 23:48:11 -0700

    final touches

--- assets/script.js
+++ assets/script.js
@@ -1,93 +1,155 @@
+//Setting variables
 var timeEl = document.querySelector(".timer");
-var startEl = document.querySelector(".startBtn");
-var p = document.getElementById("intro");
+var start = document.querySelector(".startBtn");
 var h1 = document.getElementById("mainHeading");
+var p = document.getElementById("intro");
+var div = document.querySelector(".hide");
+var buttons = document.getElementsByName("choice");
+var initials = document.getElementById("initials");
+var submit = document.getElementById("submit");
 
-var buttons = document.createElement("div");
-var incor = document.createElement("h2");
+//Buttons
+var opt1 = document.getElementById("opt-1");
+var opt2 = document.getElementById("opt-2");
+var opt3 = document.getElementById("opt-3");
+var opt4 = document.getElementById("opt-4");
 
+var scoreBoard = [];
 
-incor.classList.add("hidden");
-incor.textContent = "Try again!";
+start.addEventListener("click", startGame);
 
-startEl.addEventListener("click", startGame);
+const quiz = {
+    question1: "Question 1: HTML stands for:",
+    question2: "Question 2: What git command do you use to create a file in your directory?",
+    question3: "Question 3: What does the * selector in CSS do?",
+    question4: "Question 4: Which one of these is an example of a browser API?",
+    question5: "Question 5: Where do you link your jQuery library in your HTML file?"
+}
+
+//runs when wrong button is clicked
+function wrong() {
+    this.setAttribute("class", "red");
+    if (secondsLeft >= 15){
+        secondsLeft = secondsLeft - 15;
+    } else {
+        secondsLeft = 0;
+        clearInterval(timeInterval);
+    }
+    
+}
+
+//runs at the beginning of each question to reset
+function reset() {
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute("class", "reveal");
+    }
+    secondsLeft = secondsLeft + 15;
+}
 
 var secondsLeft = 100;
 
-function startGame () {
-startTimer();
-Q1();
-}
+function startGame() {
+    var timeInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "Time left: " + secondsLeft;
+    
+        if(secondsLeft <= 0) {
+            clearInterval(timeInterval);
+            div.setAttribute("class", "hide");
+            h1.textContent = "Oof.. your score is " + secondsLeft
+        }
+        }, 1000);
 
 
-
-function Q1() { 
-   p.remove();
-   startEl.remove();
-   h1.textContent = "Question 1: HTML stands for:";
-
-  
-buttons.insertAdjacentHTML("beforeend", `
-    <button>Hypertext Machine Learning</button>
-    <button>Hypertext Markup Language</button>
-    <button>Hard To Master Language</button>
-    <button>Hypotext Margin Lucidness</button>
-`);
-h1.append(buttons);
-buttons.classList.add("buttonContainer");
-buttons.childNodes[3].addEventListener("click", Q2, {once: true});
-
-}
+    p.remove();
+    start.remove();
+    div.setAttribute("class", "show");
+    
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute("class", "reveal");
+   
+    }
+    
+    h1.textContent = quiz.question1;
+    opt1.textContent = "A. Hypertext Machine Learning"
+    opt2.textContent = "B. Hypertext Markup Language"
+    opt3.textContent = "C. Hard To Master Language"
+    opt4.textContent = "D. Hypotext Margin Lucidness"
+    opt2.addEventListener("click", Q2, {once: true});
+    opt1.addEventListener("click", wrong);
+    opt3.addEventListener("click", wrong);
+    opt4.addEventListener("click", wrong);
 
 
 function Q2() {
-    h1.textContent = "Question 2: What git command do you use to create a file in your directory?"
-    h1.append(buttons);
-    buttons.children[0].textContent = "A. git add .";
-    buttons.children[1].textContent = "B. git create";
-    buttons.children[2].textContent = "C. touch";
-    buttons.children[3].textContent = "D. new file";
-    buttons.childNodes[5].addEventListener("click", Q3, {once: true});
-
+    reset();
+    h1.textContent = quiz.question2;
+    opt1.textContent = "A. git add ."
+    opt2.textContent = "B. git create"
+    opt3.textContent = "C. touch"
+    opt4.textContent = "D. new file"
+    opt3.addEventListener("click", Q3, {once: true});
+    opt1.addEventListener("click", wrong);
+    opt2.addEventListener("click", wrong);
+    opt4.addEventListener("click", wrong);
 }
 
 function Q3() {
-    h1.textContent = "Question 3: What does the * selector in CSS do?"
-    h1.append(buttons);
-    buttons.children[0].textContent = "A. nothing";
-    buttons.children[1].textContent = "B. target one element";
-    buttons.children[2].textContent = "C. make it look pretty";
-    buttons.children[3].textContent = "D. targets every element";
-    buttons.childNodes[7].addEventListener("click", Q4, {once: true});
+    reset();
+    h1.textContent = quiz.question3;
+    opt1.textContent = "A. nothing"
+    opt2.textContent = "B. targets one element"
+    opt3.textContent = "C. makes it look pretty"
+    opt4.textContent = "D. targets every element"
+    opt4.addEventListener("click", Q4, {once: true});
+    opt1.addEventListener("click", wrong);
+    opt3.addEventListener("click", wrong);
+    opt2.addEventListener("click", wrong);
 }
 
 function Q4() {
-    h1.textContent = "Question 4: Which one of these is an example of a browser API?"
-    h1.append(buttons);
-    buttons.children[0].textContent = "A. GoogleFonts";
-    buttons.children[1].textContent = "B. JSON";
-    buttons.children[2].textContent = "C. jQuery";
-    buttons.children[3].textContent = "D. Bootstrap";
-    buttons.childNodes[3].addEventListener("click", Q5, {once: true});
+    reset();
+    h1.textContent = quiz.question4;
+    opt1.textContent = "A. GoogleFonts"
+    opt2.textContent = "B. JSON"
+    opt3.textContent = "C. jQuery"
+    opt4.textContent = "D. Bootstrap"
+    opt2.addEventListener("click", Q5, {once: true});
+    opt1.addEventListener("click", wrong);
+    opt3.addEventListener("click", wrong);
+    opt4.addEventListener("click", wrong);
 }
 
 function Q5() {
-    h1.textContent = "Question 5: Where do you link your jQuery library in your HTML file?"
-    h1.append(buttons);
-    buttons.children[0].textContent = "A. in the head";
-    buttons.children[1].textContent = "B. at the top of the body";
-    buttons.children[2].textContent = "C. at the bottom of the body above the JavaScript link";
-    buttons.children[3].textContent = "D. at the bottom of the body below the JavaScript link";
-    buttons.childNodes[5].addEventListener("click", console.log("You won!"));
+    reset();
+    h1.textContent = quiz.question5;
+    opt1.textContent = "A. In the head of the HTML"
+    opt2.textContent = "B. At the top of the body"
+    opt3.textContent = "C. At the bottom of the body above the JavaScript link"
+    opt4.textContent = "D. At the bottom of the body below the JavaScript Link"
+    opt1.addEventListener("click", wrong);
+    opt2.addEventListener("click", wrong);
+    opt4.addEventListener("click", wrong);
+    opt3.addEventListener("click", function() {
+        h1.textContent = "Great Job! Your score is " + secondsLeft;
+        clearInterval(timeInterval);
+        timeEl.textContent = "";
+        div.setAttribute("class", "hide");
+        document.querySelector("input").setAttribute("class", "form");
+        document.querySelector(".enter").setAttribute("class", "form");
+        submit.addEventListener("click", recordScore);
+    })
+}}
+
+function recordScore() {
+    h1.textContent = "Score Board:"
+    submit.remove();
+    initials.remove();
+    var score = initials.value;
+    scoreBoard.push(score);
+    var h3 = document.createElement("h3");
+    h3.textContent = scoreBoard + " " + secondsLeft;
+    h1.append(h3);
+    localStorage.setItem("scoreBoard", JSON.stringify(scoreBoard));
+    console.log(localStorage);
 }
-
-
-
-
-
-
-
-
-
-
-
